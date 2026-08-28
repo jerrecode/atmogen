244544e1a8721a230a5af81cc19423645179db8a
Jeremy Resch <134104915+jerrecode@users.noreply.github.com>
2026-08-28 19:04:30 +0200

    Export resolved vertical process API

--- src/atmogen/__init__.py
+++ src/atmogen/__init__.py
@@ -17,7 +17,8 @@ from .liquids import (ActivityModel, IdealActivityModel, LiquidPhaseSplitResult,
                       liquid_phase_stability, select_activity_model)
 from .models import (ColumnBatchInput, ColumnInput, ElementInventory, Fidelity,
                      LiquidPhaseState, PlanetChemistryResult, PlanetPhysicalState,
-                     SolverSettings, StellarSpectrum, SurfaceReservoirs)
+                     SolverSettings, StellarSpectrum, SurfaceReservoirs,
+                     VerticalProcessResult)
 from .optics import (FresnelReflectance, MieEfficiencies,
                      absorption_coefficient_m_inv, fresnel_reflectance,
                      lorentz_lorenz_mix, mie_sphere_efficiencies,
@@ -30,6 +31,7 @@ from .solver import solve_columns, solve_planet
 from .transport import (QuenchDiagnostic, VerticalTransportResult,
                         eddy_diffusion_flux_mol_m2_s, integrate_eddy_diffusion,
                         mixing_timescale_s, quench_diagnostic)
+from .vertical_processes import layer_thickness_from_centres, solve_vertical_processes
 from .version import API_SCHEMA_VERSION, DATA_SCHEMA_VERSION, __version__
 
 __all__ = ["API_SCHEMA_VERSION", "ActivityModel", "BUILTIN_DATABASE", "ChemicalDatabase",
@@ -40,15 +42,17 @@ __all__ = ["API_SCHEMA_VERSION", "ActivityModel", "BUILTIN_DATABASE", "ChemicalD
            "PlanetChemistryResult", "PlanetPhysicalState", "PrecipitationStepResult",
            "ProvenanceClass", "QuenchDiagnostic", "Reaction", "SedimentationResult",
            "SettlingResult", "SolverSettings", "Species", "StellarSpectrum", "SurfaceReservoirs",
-           "VerticalTransportResult", "__version__", "absorption_coefficient_m_inv",
-           "arrhenius_rate_constant", "attenuate_actinic_photon_flux", "blackbody_stellar_spectrum",
+           "VerticalProcessResult", "VerticalTransportResult", "__version__",
+           "absorption_coefficient_m_inv", "arrhenius_rate_constant",
+           "attenuate_actinic_photon_flux", "blackbody_stellar_spectrum",
            "column_photolysis_rates_s1", "cunningham_slip_correction",
            "eddy_diffusion_flux_mol_m2_s", "expected_rate_coefficient_units",
            "fresnel_reflectance", "integrate_eddy_diffusion", "integrate_kinetics",
-           "liquid_mixture_density_kg_m3", "liquid_phase_stability", "lorentz_lorenz_mix",
-           "mie_sphere_efficiencies", "mixing_timescale_s", "particle_optical_coefficients",
-           "photolysis_rate_s1", "precipitation_step", "quench_diagnostic",
-           "rayleigh_sphere_efficiencies", "reaction_element_residual", "reaction_rates",
-           "sedimentation_mass_flux", "select_activity_model", "solve_columns", "solve_planet",
+           "layer_thickness_from_centres", "liquid_mixture_density_kg_m3",
+           "liquid_phase_stability", "lorentz_lorenz_mix", "mie_sphere_efficiencies",
+           "mixing_timescale_s", "particle_optical_coefficients", "photolysis_rate_s1",
+           "precipitation_step", "quench_diagnostic", "rayleigh_sphere_efficiencies",
+           "reaction_element_residual", "reaction_rates", "sedimentation_mass_flux",
+           "select_activity_model", "solve_columns", "solve_planet", "solve_vertical_processes",
            "species_moles_to_elements", "spectral_irradiance_to_photon_flux",
            "sphere_drag_coefficient", "terminal_settling_velocity"]
